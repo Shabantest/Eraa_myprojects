931b5beca24cf11161247a620b15c0038eef598b
shaban <shabanemail@email.com>
2023-12-05 17:40:43 +0200

    Assignment3 with fultter

--- lib/Screen1.dart
+++ lib/Screen1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Screen1 extends StatefulWidget {
   const Screen1({super.key});
 
@@ -9,6 +10,6 @@ class Screen1 extends StatefulWidget {
 class _Screen1State extends State<Screen1> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    return const Scaffold();
   }
 }
